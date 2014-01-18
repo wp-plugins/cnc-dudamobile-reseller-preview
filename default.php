81d602ac2bd119722e5c44d3474808554c97507d
cnckevin <cnckevin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-18 18:36:24 +0000

    added auto scroll to iframe if needed
    git-svn-id: https://plugins.svn.wordpress.org/cnc-dudamobile-reseller-preview/tags/1.3@841027 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ default.php
@@ -0,0 +1,622 @@
+<?php
+
+/**
+
+ * Plugin Name: Duda Reseller API Plugin
+
+ * Plugin URI: http://wordpress.org/plugins/cnc-dudamobile-reseller-preview/
+
+ * Description: Create Dudamobile previews instantly
+
+ * Version: 1.3
+
+ * Author: Kevin Champlin
+
+ * Author URI: http://cncwebsolutions.com
+
+ * License: GPL2
+
+ */
+
+ 
+
+ /*  Copyright 2014  CNC Web Solutions (email :kevin@cncwebsolutions.com)
+
+
+
+    This program is free software; you can redistribute it and/or modify
+
+    it under the terms of the GNU General Public License, version 2, as 
+
+    published by the Free Software Foundation.
+
+
+
+    This program is distributed in the hope that it will be useful,
+
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+    GNU General Public License for more details.
+
+
+
+    You should have received a copy of the GNU General Public License
+
+    along with this program; if not, write to the Free Software
+
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+$pluginsURI = plugins_url('/cnc-dudamobile-reseller-preview/');
+
+	wp_register_style('cnc_css', $pluginsURI . 'css/cnc-style.css', array(), '1.0' ); 
+	wp_enqueue_style( 'cnc_css' );
+
+
+
+function cnc_plugin_settings_duda($links) {
+	$settings_link = '<a href="options-general.php?page=cnc-dudamobile-reseller-preview/default.php">Settings</a>';
+	array_unshift($links,$settings_link);
+	return $links;
+	
+}
+
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'cnc_plugin_settings_duda' );
+ 
+
+ function show_duda_stuff()
+
+ {
+
+	$duda_username = get_option('duda_api_username');
+	$duda_password = get_option('duda_api_password');
+	$duda_button_text = get_option('duda_button_text');
+	$duda_debug = get_option('duda_api_debug');
+	
+	if (strlen($duda_button_text) == 0){
+		$duda_button_text = "create my mobile preview";
+	}
+	
+	
+	
+
+?>	
+
+
+
+<script>
+
+function validateForm()
+
+{
+
+var x=document.forms["preview-form"]["ifrmSite"].value;
+
+if (x==null || x=="")
+
+  {
+
+  alert("url name must be filled out");
+
+  return false;
+
+  }
+
+  
+
+  
+
+   var url = x;
+
+        var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+        if (pattern.test(url)) {
+
+			  document.forms["preview-form"]["ifrmSite"].value =  x;
+
+
+
+            //alert("Url is valid");
+
+            return true;
+
+        } 
+
+           // alert("Url is not valid!");
+
+            document.forms["preview-form"]["ifrmSite"].value = "http://" + x;
+
+			return true;
+
+
+
+}
+
+
+function ButtonClicked()
+{
+   document.getElementById("formsubmitbutton").style.display = "none"; // to undisplay
+   document.getElementById("buttonreplacement").style.display = ""; // to display
+   return true;
+}
+var FirstLoading = true;
+function RestoreSubmitButton()
+{
+   if( FirstLoading )
+   {
+      FirstLoading = false;
+      return;
+   }
+   document.getElementById("formsubmitbutton").style.display = ""; // to display
+   document.getElementById("buttonreplacement").style.display = "none"; // to undisplay
+}
+// To disable restoring submit button, disable or delete next line.
+
+
+</script>
+
+
+<div id="cncForm"> 
+<form method="post" name="preview-form"  onSubmit="validateForm()" >
+
+  
+   <strong>Enter your Website URL:</strong>
+        <input type="text" class="cnc_url" value="" name="ifrmSite" id="ifrmsite"/ > 
+  
+
+        <div id="formsubmitbutton"  class="cncSubmit" >
+            <input type="submit" class="cnc_submit" name="submit" id="submit" value=" <?=$duda_button_text; ?> "  onclick="ButtonClicked()">
+        </div>
+        <div id="buttonreplacement"  style="position:relative; margin-left:370px; top:-35px; display:none;">
+        <img src="<?= plugins_url('/cnc-dudamobile-reseller-preview/') .'images/ajax-loader.gif'?>" alt="loading..." />
+        </div>
+ 
+
+
+</form>
+</div>
+
+
+	<script>
+
+var newurl = $('#ifrmSite').val();
+
+$('#mobilepreview').attr('src', newurl);
+
+window.frames["mobilepreview"].location.reload();
+
+
+
+</script>
+
+
+
+<?php
+
+	if(isset($_POST['submit']))  
+
+{
+
+	// Create Site
+
+//	echo "Creating Site...<br/>";
+
+	
+
+	// enter your site url here. We are using moeplumbing.com as an example here
+
+	$data = '
+
+		{	
+
+		"site_data":
+
+			{						
+
+				"original_site_url":"' . $_POST['ifrmSite'] . '"
+
+			}
+
+		}
+
+	';
+
+
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, 'https://api.dudamobile.com/api/sites/create');
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	curl_setopt($ch, CURLOPT_USERPWD,  "$duda_username:$duda_password");
+
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+	curl_setopt($ch, CURLOPT_POST,1);
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+
+		'Content-Type: application/json', 		
+
+		'Content-Length: ' . strlen($data))                                                                       
+
+	);   	
+
+	$output = curl_exec($ch);
+
+	$info = curl_getinfo($ch);
+
+		
+
+	if ($info['http_code'] != 200) {
+
+		print(".<br/>Error creating site.<br/><br/>");
+
+		if ($duda_debug ==1)
+
+		{
+
+		print_r($output);
+
+		print_r($info);
+
+		}
+
+		die();
+
+	}
+
+	
+
+	// Get result site name
+
+	$output = json_decode($output, true);
+
+	$siteName = $output['site_name'];
+
+	
+
+//	echo "Site Created..<br/><br/>";		
+
+	
+
+	// Get Site
+
+//	echo "Getting site information....<br/>";
+
+	
+
+	$data = '';
+
+
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, 'https://api.dudamobile.com/api/sites/'.$siteName);
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	curl_setopt($ch, CURLOPT_USERPWD,  "$duda_username:$duda_password");
+
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+
+		'Content-Type: application/json', 		
+
+		'Content-Length: ' . strlen($data))                 
+
+	);
+
+	$output = curl_exec($ch);
+
+	$info = curl_getinfo($ch);
+
+	
+
+	if ($info['http_code'] != 200) {
+
+	//	print("<br/>Error getting site data.<br/><br/>");
+
+	//	print_r($output);
+
+	//	print_r($info);
+
+		die();
+
+	}
+
+	
+
+//	echo "Information Retrieved.<br/><br/>";
+
+	
+
+	
+
+	$output = json_decode($output, true);
+
+	$previewUrl = $output["site_extra_info"]["preview_url"];
+
+	$beforeAfterUrl = $output["site_extra_info"]["before_after_preview_url"];
+
+	
+
+//	echo "Preview URL:".$previewUrl."<br/>";
+
+//	echo "Before/After URL:".$beforeAfterUrl."<br/>";
+
+	
+
+	curl_close($ch);
+
+	echo '<iframe seamless="seamless" scrolling="auto" id="mobilepreview" width="1100" height="930" src="http://'. $beforeAfterUrl . '"></iframe>';
+
+
+
+}
+
+?>	
+
+	
+
+
+
+<?php	
+
+	
+
+	return "$preview_code"; 
+
+ }
+
+ add_shortcode( 'cnc_duda', 'show_duda_stuff' ); // old shortcode (still works for backward compatability)
+ add_shortcode( 'dudamobile_preview', 'show_duda_stuff' );  // new shortcode - will be used moving forward
+
+ 
+
+  function set_duda_options()
+ {
+	add_option('duda_api_username','api username goes here','API Username');
+	add_option('duda_api_password','api password','API Password');
+	add_option('duda_button_text','button text ','Button Text');
+	add_option('duda_api_debug','api debug','API Debug');
+
+ }
+
+ 
+
+ function unset_duda_options() {
+
+ 	delete_option('duda_api_username');
+	delete_option('duda_api_password');
+	delete_option('duda_button_text');
+	delete_option('duda_api_debug');
+
+ }
+
+
+register_activation_hook(__FILE__,'set_duda_options');
+register_deactivation_hook(__FILE__,'unset_duda_options');
+
+
+
+function modify_menu(){
+
+		add_options_page(
+
+							'CNC Duda Preview',
+
+							'CNC Duda Preview',
+
+							'manage_options',
+
+							__FILE__,
+
+							'admin_duda_options'
+
+						);
+
+	}
+
+
+
+function admin_duda_options(){
+
+	?>
+
+    <div class="wrap">
+
+    <h2>Duda Reseller API Options</h2>
+    <hr/>
+
+    <?php
+
+	if ($_REQUEST['submit']){
+
+		update_duda_options();
+
+	}
+
+	print_duda_form();
+
+    ?></div>
+
+    <?php	
+
+}
+
+
+
+function update_duda_options(){
+
+$ok = false;
+
+
+
+if ($_REQUEST['duda_api_username']){
+
+	update_option('duda_api_username',$_REQUEST['duda_api_username']); 
+
+	$ok=true;
+
+}
+
+
+
+if ($_REQUEST['duda_api_password']){
+
+	update_option('duda_api_password',$_REQUEST['duda_api_password']);
+
+	$ok=true;
+
+}
+
+if ($_REQUEST['duda_button_text']){
+
+	update_option('duda_button_text',$_REQUEST['duda_button_text']);
+
+	$ok=true;
+
+}
+
+
+	update_option('duda_api_debug',$_REQUEST['duda_api_debug']);
+
+
+
+
+
+	if($ok){
+
+		?><div id="message" class="updated fade">
+
+		<p>Options saved.</p>
+
+		</div><?php
+
+	}
+
+	else
+
+	{
+
+		?><div id="message" class="updated fade">
+
+		<p>Failed to save options.</p>
+
+		</div><?php
+
+	}
+
+		
+
+}
+
+
+
+
+
+function print_duda_form(){
+
+	$duda_api_username =  get_option('duda_api_username');
+	$duda_api_password = get_option('duda_api_password');
+	$duda_button_text = get_option('duda_button_text');
+	$duda_api_debug = get_option('duda_api_debug');
+
+	?>
+
+    This will create a form allow users to submit their url and then generates a mobile preview. <br/>
+    This mobile preview is also saved in your Duda Reseller Dashboard.<br/><br/>
+
+ 
+
+    <form action="options-general.php?page=cnc-dudamobile-reseller-preview/default.php" method="post"> 
+
+    	<label for"api_username">API Username:
+
+        	<input type="text" name="duda_api_username" value="<?=$duda_api_username?>" />
+
+        </label>
+
+        <br/>
+
+        <label for"api_password">&nbsp;&nbsp;API Password:
+
+        	<input type="text" name="duda_api_password" value="<?=$duda_api_password?>" />
+
+        </label>
+
+        <br/>
+  	<label for"duda_button_text">&nbsp;&nbsp;Button Text:
+
+        	<input   name="duda_button_text" type="text" value="<?=$duda_button_text?>" size="50" />
+
+        </label>
+
+        <br/> <br/>
+        
+
+        <label for"ap_debug">&nbsp;&nbsp;Debug:
+
+       
+
+        <input name="duda_api_debug" type="radio" value="0" <?php if ($duda_api_debug ==0 || $duda_api_debug !=1) echo "checked=checked" ?>  />no
+
+        <input type="radio" name ="duda_api_debug" value="1" <?php if ($duda_api_debug ==1) echo "checked=checked" ?> />yes
+
+        </label>
+
+         <br/><br/>
+
+        <input type ="submit" name ="submit" value=" Save " />
+
+    </form>
+
+    
+
+    <br/><br/>
+
+    <hr/>
+
+    If you found this plugin useful please consider donating.   
+
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+
+<input type="hidden" name="cmd" value="_s-xclick">
+
+<input type="hidden" name="hosted_button_id" value="J5RLUBJ9CFP4E">
+
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+
+</form>
+
+<br/><br/>
+
+    If you need hosting, domains, design or custom programming <a href="mailto:kevin@cncwebsolutions.com">contact us</a><br/>
+    <strong><a href="http://cncwebsolutions.com/1-web-hosting/" target="_blank">WordPress hosting starting at just $1.</a></strong>
+
+    <?php
+
+}
+
+	
+
+add_action('admin_menu','modify_menu');
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,147 @@
+=== Plugin Name ===
+
+Contributors: cnckevin
+
+Tags: dudamobile, reseller
+
+Requires at least: 3.8
+
+Tested up to: 3.8
+
+Stable tag: 1.3
+
+License: GPLv2 or later
+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Add dudamobile previews on your wordpress page easy by using a shortcode.
+
+
+
+== Description ==
+
+This plugin is for dudamobile resellers that have API access. It allows you to enter your api username and api password in the settings.  Then you can use a shortcode to display the mobile preview on any page.  This was only tested on WordPress version 3.8 so I cannot confirm if it works on any past versions.  If it does please let me know.
+
+
+All you need to make this work is to add the short code: [dudamobile_preview] to your page.  Remember to add your username and password under Settings in the admin.
+
+
+
+
+
+
+
+
+
+== Installation ==
+
+
+1. Click install or upload `cnc-duda-reseller` to the `/wp-content/plugins/` directory
+
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+1. Go to the admin and click on settings then choose 'CNC Duda Preview'. Enter your API Username & Password here
+
+1. Place the shortcode [dudamobile_preview] on any page you would like to show the mobile preview.
+
+
+
+== Frequently Asked Questions ==
+
+
+
+= Does this work with any Dudamobile plan =
+It only works if you have a plan that has the Duda API access.
+
+= It won't work, what could be wrong? =
+In the settings you can turn debugging on or off.  If you turn it on you will see the debugging error message returned from the API. Common
+errors are not updating the API Username & Password in the settings.
+
+= Do I have to edit code to make this work? =
+Absolutely not! Just install the plugin, add your username & password in the settings and then use the shortcode.  Simple!
+
+
+= Does this use all the API functions =
+Not yet, this only creates the mobile comparison preview.  I will add more features as time allows.
+
+= It just isn't working, what can I do? =
+The best thing is to install an error log plugin and look at the error log.  If you have an error log you can send it to kevin@cncwebsolutions.com
+
+
+
+
+== Screenshots ==
+
+1. Just add your username & password and save
+
+2. Textbox to type the website to convert
+
+3. Pretty comparison embedded in your site
+
+
+
+
+
+
+== Changelog ==
+
+= 1.3 =
+
+
+* Allowed the iframe source to scroll if the site is to small to containthe preview of 1100px.
+
+
+
+= 1.2 =
+
+
+* Fixed a bug with the embed tag
+
+
+
+= 1.1 =
+
+
+
+* Added custom button text
+
+* Added new styles form
+
+* Added new shortcode [dudamobile_preview]  (old shortcode still works)
+
+= 1.0 =
+
+Version 1 was released. 
+
+* Added a loading image after the form is submitted
+
+* Some css fixes
+
+* Added Settings page on the plugins page
+
+* Added better installation and FAQ pages
+
+
+
+
+= 0.2 =
+
+Added debug option in settings to see the error returned by dudamobile api
+
+
+
+= 0.1 =
+
+Initial release
+
+
+
+== Arbitrary section ==
+
+* Settings page to add your api username & password.  Also turn debugging on and off.
+
+* Shortcode to add your mobile preview on any page [dudamobile_preview]
+
+
+
